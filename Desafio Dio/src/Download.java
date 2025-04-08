cb6fbee465ff6dda7d90fe09aa5bb9956e4398fe
Eduardo-Barros08 <edubarsilva29@gmail.com>
2025-04-07 20:50:31 -0400

    Desafio Download feito

---
+++ Desafio Dio/src/Download.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; 
+    
+public class Download {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int tamanho = leitor.nextInt();
+        
+        // TODO: Crie a condição necessária para que, de acordo com o tamanho, seja printado no console barras representando o download
+        // Exemplo: se o tamanho for 5, deve ser impresso "|||||"
+        // Exemplo: se o tamanho for 10, deve ser impresso "||||||||||"
+        
+        if(tamanho > 0 && tamanho <= 10)
+        for(int i = 0; i < tamanho; i++){
+            System.out.print("/");
+        }
+        else if(tamanho > 10){
+            System.out.println("Número inválido!");
+        }
+    }
+}
